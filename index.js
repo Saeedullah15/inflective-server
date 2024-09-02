7f5d1c1a490858363a12e4f16b08d3387d7f7763
Saeedullah15 <saeedullahazim65@gmail.com>
2024-09-03 00:05:09 +0600

    query details api implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -48,6 +48,13 @@ async function run() {
             };
             const cursor = await myQueryCollection.find(query, options).toArray();
             res.send(cursor);
+        })
+
+        app.get("/queryDetails/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await myQueryCollection.findOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
